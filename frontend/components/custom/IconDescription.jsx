9c2003fabccd8ed3d4120455052d6f03b54cf916
Diego <99410932+DrGomez21@users.noreply.github.com>
2026-02-28 17:13:04 -0300

    Add casos de uso

--- frontend/components/custom/IconDescription.jsx
+++ frontend/components/custom/IconDescription.jsx
@@ -1,8 +1,28 @@
-export const IconDescription = ({ icon, description }) => {
+export const IconDescription = ({ icon, description, type }) => {
+
+  if (type === "titulo") {
+    return (
+      <div className="w-full flex gap-x-1 items-center text-slate-900">
+        {icon}
+        <p className="text-sm font-medium">{description}</p>
+      </div>
+    )
+  }
+
+  if (type === "detalle") {
+    return (
+      <div className="w-full flex gap-x-1 items-center text-slate-500">
+        {icon}
+        <p className="text-slate-500 text-sm">{description}</p>
+      </div>
+    )
+  }
+
   return (
     <div className="w-full flex gap-x-1 items-center text-slate-500">
       {icon}
       <p className="text-slate-500 text-sm">{description}</p>
     </div>
   )
+
 }
